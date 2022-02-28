36fe8c9cf6f3ec1b4eaed35156d4dddb8f5dea73
brittany melton <brittanymelton26@gmail.com>
2022-02-28 16:26:16 -0500

    completed exercise

--- areaOfACircle-operatorsExercise2/Program.cs
+++ areaOfACircle-operatorsExercise2/Program.cs
@@ -6,7 +6,23 @@ namespace areaOfACircle_operatorsExercise2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //In class exercise
+            /*Create a method that will Calculate the area of a circle based on its radius
+             * Allow a user to input a value for r in the console */
+
+            Console.WriteLine("\n\nwhat is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"\nThe area of a circle with radius of {radius} is {area}");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+
+            return Math.PI * radius * radius;
+
         }
     }
+    
 }
